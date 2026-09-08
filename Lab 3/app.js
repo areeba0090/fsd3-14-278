db67e806e3c649fce8c419f61c92e8cd5712aba8
Areeba Alam <areeba.25b01010090@abes.ac.in>
2026-09-08 09:37:23 +0530

    addTeams.js

--- Lab 3/app.js
+++ Lab 3/app.js
@@ -1,9 +1,9 @@
 import http from "http";
 
 const server = http.createServer((req,res) => {   // turn on the server and the number of times request comes respond 
-    res.write("<h2>Welcome to Serverside</h2>");
+    res.end("<h1>SIH Internal</h1>")
 });
 
 server.listen(5000, ()=>{
-    console.log("Server is Running");
+    console.log("SIH Server is Running");
 });
