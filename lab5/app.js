e97f9602ff7e1895f584b27cad5c1759add44e9a
Areeba Alam <areeba.25b01010090@abes.ac.in>
2026-09-17 13:59:50 +0530

    14:00

--- lab5/app.js
+++ lab5/app.js
@@ -6,7 +6,28 @@ app.get("/", (req,res) => {
     res.send("<h1>Hello Express</h1>");
 });
 
-app.use("*", (req,res) => {
-    res.send("route not found");
+app.get('/about',(req,res)=>{
+    res.send("We are FSD Developer")
 })
-app.listen(3000,()=>console.log("Server is Running"));
+
+app.post('/login',(req,res)=>{
+     res.send({msg:'user login'})
+})
+
+app.put('/user/update/1',(req,res)=>{
+    res.send({msg:'user update'})
+})
+
+app.delete('/users/1',(req,res)=>{
+    res.send({msg:'remove user 1'})
+})
+
+app.use((req,res)=>{
+    res.status(404).send("Not found")
+})
+
+app.listen(3333,() => console.log("Server running at 3333"));
+
+// server.on("error",(err) => {
+//     console.error("Server listen error:",err);
+// })
---
+++ lab5/readme.md
@@ -0,0 +1,16 @@
+# Express
+Fast, unopiniated, minimalist web framework for Node.js
+
+1. Create npm project `npm init -y`
+2. Update package.json 
+    `type:module`
+3. Install nodemon with `npm i nodemon -D`
+4. Install express with `npm i express`
+5. Update package.json
+
+...
+"scripts": {
+    "start":"node app.js"
+    "dev":"nodemon app.js"
+},
+...
